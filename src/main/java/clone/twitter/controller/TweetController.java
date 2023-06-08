5ce7430e366027a106a49197f867cedea4aa9e98
JO <71416000+insightp25@users.noreply.github.com>
2023-06-09 06:39:13 +0900

    first spring rest docs tryout, switched to springboot version 3 and edited related dependencies

--- src/main/java/clone/twitter/controller/TweetController.java
+++ src/main/java/clone/twitter/controller/TweetController.java
@@ -6,10 +6,10 @@ import clone.twitter.domain.Tweet;
 import clone.twitter.dto.request.TweetLoadRequestDto;
 import clone.twitter.dto.request.TweetComposeRequestDto;
 import clone.twitter.service.TweetService;
+import jakarta.validation.Valid;
 import java.net.URI;
 import java.util.List;
 import java.util.Optional;
-import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -35,29 +35,29 @@ public class TweetController {
     @Autowired
     private final ModelMapper modelMapper;
 
-    /**
-     * 타임라인의 최초 트윗 목록 조회 요청을 처리합니다(이후 API 다음 버전에서 아래의 getNextTweets() 메서드와 합치도록 리팩토링 예정).
-     */
-    @GetMapping("/timeline")
-    public List<Tweet> getInitialTweets(@RequestBody String userId) {
-        return tweetService.getInitialTweets(userId);
-    }
-
-    /**
-     * 이전 트윗목록의 끝까지 모두 조회했을 시 트윗목록을 추가 조회 요청을 처리합니다(이후 API 다음 버전에서 위의 getInitialTweets() 메서드와 합치도록 리팩토링 예정).
-     */
-    @GetMapping("/timeline/next")
-    public List<Tweet> getNextTweets(@RequestBody @Valid TweetLoadRequestDto tweetLoadRequestDto) {
-        return tweetService.getNextTweets(tweetLoadRequestDto.getUserId(), tweetLoadRequestDto.getCreatedAtOfLastTweet());
-    }
-
-    /**
-     * 트윗의 상세내용 조회 요청을 처리합니다.
-     */
-    @GetMapping("/{tweetId}")
-    public Optional<Tweet> getTweet(@PathVariable String tweetId) {
-        return tweetService.getTweet(tweetId);
-    }
+//    /**
+//     * 타임라인의 최초 트윗 목록 조회 요청을 처리합니다(이후 API 다음 버전에서 아래의 getNextTweets() 메서드와 합치도록 리팩토링 예정).
+//     */
+//    @GetMapping("/timeline")
+//    public List<Tweet> getInitialTweets(@RequestBody String userId) {
+//        return tweetService.getInitialTweets(userId);
+//    }
+//
+//    /**
+//     * 이전 트윗목록의 끝까지 모두 조회했을 시 트윗목록을 추가 조회 요청을 처리합니다(이후 API 다음 버전에서 위의 getInitialTweets() 메서드와 합치도록 리팩토링 예정).
+//     */
+//    @GetMapping("/timeline/next")
+//    public List<Tweet> getNextTweets(@RequestBody @Valid TweetLoadRequestDto tweetLoadRequestDto) {
+//        return tweetService.getNextTweets(tweetLoadRequestDto.getUserId(), tweetLoadRequestDto.getCreatedAtOfLastTweet());
+//    }
+//
+//    /**
+//     * 트윗의 상세내용 조회 요청을 처리합니다.
+//     */
+//    @GetMapping("/{tweetId}")
+//    public Optional<Tweet> getTweet(@PathVariable String tweetId) {
+//        return tweetService.getTweet(tweetId);
+//    }
 
     /**
      * 트윗 포스팅 요청을 처리합니다.
@@ -88,11 +88,11 @@ public class TweetController {
         return ResponseEntity.created(createdUri).body(tweetEntityModel);
     }
 
-    /**
-     * 트윗 삭제 요청을 처리합니다.
-     */
-    @DeleteMapping("/{tweetId}")
-    public void deleteTweet(@PathVariable String tweetId) {
-        tweetService.deleteTweet(tweetId);
-    }
+//    /**
+//     * 트윗 삭제 요청을 처리합니다.
+//     */
+//    @DeleteMapping("/{tweetId}")
+//    public void deleteTweet(@PathVariable String tweetId) {
+//        tweetService.deleteTweet(tweetId);
+//    }
 }
--- src/test/java/clone/twitter/controller/TweetControllerTest.java
+++ src/test/java/clone/twitter/controller/TweetControllerTest.java
@@ -6,8 +6,8 @@ import static org.springframework.restdocs.headers.HeaderDocumentation.responseH
 import static org.springframework.restdocs.hypermedia.HypermediaDocumentation.linkWithRel;
 import static org.springframework.restdocs.hypermedia.HypermediaDocumentation.links;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
-import static org.springframework.restdocs.payload.PayloadDocumentation.relaxedResponseFields;
 import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
 import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -25,13 +25,12 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
-import org.hamcrest.Matchers;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.extension.RegisterExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -40,17 +39,22 @@ import org.springframework.context.annotation.Import;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.transaction.support.DefaultTransactionDefinition;
+import org.springframework.web.context.WebApplicationContext;
 
-@Import(RestDocsConfiguration.class)
+@ExtendWith(RestDocumentationExtension.class)
+@Import(RestDocsConfiguration.class) // for http body json formatting customization
+@AutoConfigureMockMvc
+@AutoConfigureRestDocs // Junit5에서는 작동하지 않는다?
 @Transactional
 @SpringBootTest
-@AutoConfigureMockMvc
-@AutoConfigureRestDocs
 class TweetControllerTest {
     /**
      * springboot를 사용중일 시 mapping jackson json이 의존성으로 설정돼있으면 ObjectMapper가 자동으로 bean으로 등록
@@ -69,24 +73,33 @@ class TweetControllerTest {
 
     TransactionStatus status;
 
+    /**
+     * tweet table의 user_id 필드의 foreign key constraint으로 인해 더미 user 객체도 각 테스트 전에 생성 필요
+     */
     @BeforeEach
-    void beforeEach() {
+    void saveDummyUser() {
         status = transactionManager.getTransaction(new DefaultTransactionDefinition());
 
-        // tweet table의 user_id 필드의 foreign key constraint으로 인해 더미 user 객체도 각 테스트 전에 생성 필요
         User user = new User("idOfHarry", "harry", "harry@gmail.com", "b03b29", "hello", LocalDate.of(1999, 9, 9), LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS), LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS));
 
         userRepository.save(user);
     }
 
+    @BeforeEach
+    void setUp(WebApplicationContext context, RestDocumentationContextProvider restDocumentation) {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(context)
+            .apply(documentationConfiguration(restDocumentation))
+            .build();
+    }
+
     @AfterEach
-    void afterEach() {
+    void rollBack() {
         transactionManager.rollback(status);
     }
 
     @Test
     @DisplayName("POST /tweets - 정상적인 트윗 포스팅 케이스")
-    void composeTweetCorrectInputWithDto() throws Exception {
+    void composeTweet() throws Exception {
         TweetComposeRequestDto tweetComposeDto = TweetComposeRequestDto.builder()
             .text("hello, this is my first tweet.")
             .userId("idOfHarry")
@@ -134,110 +147,112 @@ class TweetControllerTest {
         ;
     }
 
-    /**
-     * 아래 postTweetBadRequest() 테스트와 병행 불가. application.yml에서 spring.jackson.deserialization.fail-on-unknown-properties 설정 조정 필요.
-     * @see #composeTweetBadRequest()
-     */
-    @Test
-    @DisplayName("POST /tweets - 트윗에 받기로한 필드 외 불명의 더미 필드(properties)와 데이터가 같이 들어올 경우 받기로 한 값 외 무시하고 정상처리")
-    void composeTweetExcessiveInput() throws Exception {
-        Tweet tweet = Tweet.builder()
-            .id("1")
-            .text("hello, this is my first tweet.")
-            .userId("idOfHarry")
-            .createdAt(LocalDateTime.of(2023, 6, 1, 1, 1, 1))
-            .build();
-
-        mockMvc.perform(post("/tweets")
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .accept(MediaTypes.HAL_JSON)
-                .content(objectMapper.writeValueAsString(tweet)))
-            .andDo(print()) // 어떤 요청과 응답이 오갔는지 테스트 로그에서 확인 가능
-            .andExpect(status().isCreated()) // 201이라고 직접 입력하는 것보다 type-safe
-            .andExpect(jsonPath("id").exists())
-            .andExpect(header().exists(HttpHeaders.LOCATION))
-            .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaTypes.HAL_JSON_VALUE))
-            // 아래 코드: tweet의 id, createdAt 필드들은 상관없는(TweetComposeRequestDto의 필드에 없는) 더미 데이터가 요청으로 들어와도 무시되어야 한다
-            .andExpect(jsonPath("id").value(Matchers.not("1")))
-            .andExpect(jsonPath("createdAt").value(Matchers.not(LocalDateTime.of(2023, 6, 1, 1, 1, 1))));
-    }
-
-    /**
-     * 위 postTweetExcessiveInput()와 테스트 병행 불가. application.yml에서 spring.jackson.deserialization.fail-on-unknown-properties 설정 조정 필요.
-     * @see #composeTweetExcessiveInput()
-     */
-    @Test
-    @DisplayName("POST /tweets - 트윗에 받기로한 필드 외 불명의 더미 필드(properties) 데이터가 같이 들어올 경우 bad request로 응답")
-    void composeTweetBadRequest() throws Exception {
-        Tweet tweet = Tweet.builder()
-            .id("1")
-            .text("hello, this is my first tweet.")
-            .userId("idOfHarry")
-            .createdAt(LocalDateTime.of(2023, 6, 1, 1, 1, 1))
-            .build();
-
-        // 원래대로면 결과는 이전 테스트(postTweetExcessiveInput)와 동일(201 반환) -> springboot가 제공하는 properties를 활용한 object mapper 확장기능 사용(401 반환)
-        mockMvc.perform(post("/tweets")
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .accept(MediaTypes.HAL_JSON)
-                .content(objectMapper.writeValueAsString(tweet)))
-            .andDo(print()) // 어떤 요청과 응답이 오갔는지 테스트 로그에서 확인 가능
-            .andExpect(status().isBadRequest()); // 400이라고 직접 입력하는 것보다 type-safe
-    }
-
-    @Test
-    @DisplayName("POST /tweets - 트윗에 받기로한 필드의 종류와 갯수가 일치하나 값이 비어있는 경우 bad request로 응답")
-    void composeTweetBadRequestEmptyInput() throws Exception {
-        TweetComposeRequestDto tweetComposeDto = TweetComposeRequestDto.builder().build();
-
-        this.mockMvc.perform(post("/tweets")
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .content(this.objectMapper.writeValueAsString(tweetComposeDto)))
-            .andExpect(status().isBadRequest());
-    }
-
-    /**
-     * <ul>
-     *     <li>현재 해당사항 없음. 해당 시 별도 validator 클래스 규정(Errors.rejectValue()...) 후 @Component로 빈 등록하여 test 가능.</li>
-     *     <ul>
-     *         <li>해당 경우 eg.1. 실제 포스팅을 한 userId가 아닌 이상한 유저의 id가 들어오는 경우(유저인증 파트에서 해결 예정)</li>
-     *         <li>해당 경우 eg.2. 트윗내 투표기능의 기간 설정 시 시작날짜가 종료날짜보다 늦는 경우 등</li>
-     *     </ul>
-     * </ul>
-     * @see clone.twitter.controller.TweetValidator
-     * @see clone.twitter.common.ErrorSerializer
-     */
-    @Test
-    @DisplayName("POST /tweets - 트윗에 받기로한 필드의 종류의 갯수가 일치하고 값이 있으나, 해당 값이 비즈니스 로직상 이상한 경우")
-    void composeTweetBadRequestWrongInput() throws Exception {
-        //TweetComposeRequestDto tweetComposeDto = TweetComposeRequestDto.builder()
-        //    .text("hello, this is my first tweet.")
-        //    .userId("strangeUserId")
-        //    .build();
-        //
-        //this.mockMvc.perform(post("/tweets")
-        //    .contentType(MediaType.APPLICATION_JSON_VALUE)
-        //    .content(this.objectMapper.writeValueAsString(tweetComposeDto)))
-        //    .andDo(print())
-        //    .andExpect(status().isBadRequest())
-        //    .andExpect(jsonPath("$[0].objectName").exists())
-        //    .andExpect(jsonPath("$[0].defaultMessage").exists())
-        //    .andExpect(jsonPath("$[0].code").exists());
-    }
-
+//
+////    /**
+////     * 아래 postTweetBadRequest() 테스트와 병행 불가. application.yml에서 spring.jackson.deserialization.fail-on-unknown-properties 설정 조정 필요.
+////     * @see #composeTweetBadRequest()
+////     */
+////    @Test
+////    @DisplayName("POST /tweets - 트윗에 받기로한 필드 외 불명의 더미 필드(properties)와 데이터가 같이 들어올 경우 받기로 한 값 외 무시하고 정상처리")
+////    void composeTweetExcessiveInput() throws Exception {
+////        Tweet tweet = Tweet.builder()
+////            .id("1")
+////            .text("hello, this is my first tweet.")
+////            .userId("idOfHarry")
+////            .createdAt(LocalDateTime.of(2023, 6, 1, 1, 1, 1))
+////            .build();
+////
+////        mockMvc.perform(post("/tweets")
+////                .contentType(MediaType.APPLICATION_JSON_VALUE)
+////                .accept(MediaTypes.HAL_JSON)
+////                .content(objectMapper.writeValueAsString(tweet)))
+////            .andDo(print()) // 어떤 요청과 응답이 오갔는지 테스트 로그에서 확인 가능
+////            .andExpect(status().isCreated()) // 201이라고 직접 입력하는 것보다 type-safe
+////            .andExpect(jsonPath("id").exists())
+////            .andExpect(header().exists(HttpHeaders.LOCATION))
+////            .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaTypes.HAL_JSON_VALUE))
+////            // 아래 코드: tweet의 id, createdAt 필드들은 상관없는(TweetComposeRequestDto의 필드에 없는) 더미 데이터가 요청으로 들어와도 무시되어야 한다
+////            .andExpect(jsonPath("id").value(Matchers.not("1")))
+////            .andExpect(jsonPath("createdAt").value(Matchers.not(LocalDateTime.of(2023, 6, 1, 1, 1, 1))));
+////    }
+//
+//    /**
+//     * 위 postTweetExcessiveInput()와 테스트 병행 불가. application.yml에서 spring.jackson.deserialization.fail-on-unknown-properties 설정 조정 필요.
+//     * @see #composeTweetExcessiveInput()
+//     */
 //    @Test
-//    void getInitialTweets() {
+//    @DisplayName("POST /tweets - 트윗에 받기로한 필드 외 불명의 더미 필드(properties) 데이터가 같이 들어올 경우 bad request로 응답")
+//    void composeTweetBadRequest() throws Exception {
+//        Tweet tweet = Tweet.builder()
+//            .id("1")
+//            .text("hello, this is my first tweet.")
+//            .userId("idOfHarry")
+//            .createdAt(LocalDateTime.of(2023, 6, 1, 1, 1, 1))
+//            .build();
+//
+//        // 원래대로면 결과는 이전 테스트(postTweetExcessiveInput)와 동일(201 반환) -> springboot가 제공하는 properties를 활용한 object mapper 확장기능 사용(401 반환)
+//        mockMvc.perform(post("/tweets")
+//                .contentType(MediaType.APPLICATION_JSON_VALUE)
+//                .accept(MediaTypes.HAL_JSON)
+//                .content(objectMapper.writeValueAsString(tweet)))
+//            .andDo(print()) // 어떤 요청과 응답이 오갔는지 테스트 로그에서 확인 가능
+//            .andExpect(status().isBadRequest()); // 400이라고 직접 입력하는 것보다 type-safe
 //    }
 //
 //    @Test
-//    void getNextTweets() {
+//    @DisplayName("POST /tweets - 트윗에 받기로한 필드의 종류와 갯수가 일치하나 값이 비어있는 경우 bad request로 응답")
+//    void composeTweetBadRequestEmptyInput() throws Exception {
+//        TweetComposeRequestDto tweetComposeDto = TweetComposeRequestDto.builder().build();
+//
+//        this.mockMvc.perform(post("/tweets")
+//                .contentType(MediaType.APPLICATION_JSON_VALUE)
+//                .content(this.objectMapper.writeValueAsString(tweetComposeDto)))
+//            .andExpect(status().isBadRequest());
 //    }
 //
+//    /**
+//     * <ul>
+//     *     <li>현재 해당사항 없음. 해당 시 별도 validator 클래스 규정(Errors.rejectValue()...) 후 @Component로 빈 등록하여 test 가능.</li>
+//     *     <ul>
+//     *         <li>해당 경우 eg.1. 실제 포스팅을 한 userId가 아닌 이상한 유저의 id가 들어오는 경우(유저인증 파트에서 해결 예정)</li>
+//     *         <li>해당 경우 eg.2. 트윗내 투표기능의 기간 설정 시 시작날짜가 종료날짜보다 늦는 경우 등</li>
+//     *     </ul>
+//     * </ul>
+//     * @see clone.twitter.controller.TweetValidator
+//     * @see clone.twitter.common.ErrorSerializer
+//     */
 //    @Test
-//    void getTweet() {
+//    @DisplayName("POST /tweets - 트윗에 받기로한 필드의 종류의 갯수가 일치하고 값이 있으나, 해당 값이 비즈니스 로직상 이상한 경우")
+//    void composeTweetBadRequestWrongInput() throws Exception {
+//        //TweetComposeRequestDto tweetComposeDto = TweetComposeRequestDto.builder()
+//        //    .text("hello, this is my first tweet.")
+//        //    .userId("strangeUserId")
+//        //    .build();
+//        //
+//        //this.mockMvc.perform(post("/tweets")
+//        //    .contentType(MediaType.APPLICATION_JSON_VALUE)
+//        //    .content(this.objectMapper.writeValueAsString(tweetComposeDto)))
+//        //    .andDo(print())
+//        //    .andExpect(status().isBadRequest())
+//        //    .andExpect(jsonPath("$[0].objectName").exists())
+//        //    .andExpect(jsonPath("$[0].defaultMessage").exists())
+//        //    .andExpect(jsonPath("$[0].code").exists());
 //    }
 //
-//    @Test
-//    void deleteTweet() {
-//    }
+////    @Test
+////    void getInitialTweets() {
+////    }
+////
+////    @Test
+////    void getNextTweets() {
+////    }
+////
+////    @Test
+////    void getTweet() {
+////    }
+////
+////    @Test
+////    void deleteTweet() {
+////    }
+
 }
