a5a7dfb15f770c5677cf547894720450087a397c
JO <71416000+insightp25@users.noreply.github.com>
2023-06-13 10:03:29 +0900

    added builder to user entity, more settings to base-controller-test class, and basic form of user-controller-test class

--- src/test/java/clone/twitter/common/BaseControllerTest.java
+++ src/test/java/clone/twitter/common/BaseControllerTest.java
@@ -1,6 +1,8 @@
 package clone.twitter.common;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,7 +12,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
 
 @ActiveProfiles("test")
 @Import(RestDocsConfiguration.class)
@@ -28,4 +33,19 @@ public class BaseControllerTest {
 
     @Autowired
     protected ModelMapper modelMapper;
+
+    @Autowired
+    protected PlatformTransactionManager transactionManager;
+
+    protected TransactionStatus status;
+
+    @BeforeEach
+    protected void setTransactionManager() {
+        status = transactionManager.getTransaction(new DefaultTransactionDefinition());
+    }
+
+    @AfterEach
+    protected void rollback() {
+        transactionManager.rollback(status);
+    }
 }
--- src/test/java/clone/twitter/controller/UserControllerTest.java
+++ src/test/java/clone/twitter/controller/UserControllerTest.java
@@ -1,7 +1,37 @@
 package clone.twitter.controller;
 
 import clone.twitter.common.BaseControllerTest;
+import clone.twitter.repository.UserRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class UserControllerTest extends BaseControllerTest {
+    @Autowired
+    UserRepository userRepository;
 
+    @Test
+    @DisplayName("POST /users/new - 회원가입: 정상 응답")
+    void signUp() {
+    }
+
+    @Test
+    @DisplayName("GET /users/{id} - user id로 회원 조회: 정상 응답")
+    void getUser() {
+    }
+
+    @Test
+    @DisplayName("POST /users/signin - username으로 로그인: 정상 응답")
+    void signInByUsername() {
+    }
+
+    @Test
+    @DisplayName("POST /users/signin - email로 로그인: 정상 응답")
+    void signInByEmail() {
+    }
+
+    @Test
+    @DisplayName("DELETE /users/{id} - user id로 계정 삭제: 정상 응답")
+    void deleteAccount() {
+    }
 }
