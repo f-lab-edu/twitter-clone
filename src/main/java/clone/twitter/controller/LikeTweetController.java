2afa9309ffa959dfac36a102fc4cf1bcd43da0ec
JO <71416000+insightp25@users.noreply.github.com>
2023-06-16 22:55:33 +0900

    GET users-liked-tweet

--- src/main/java/clone/twitter/controller/LikeTweetController.java
+++ src/main/java/clone/twitter/controller/LikeTweetController.java
@@ -1,14 +1,23 @@
 package clone.twitter.controller;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
 import clone.twitter.domain.LikeTweet;
 import clone.twitter.dto.response.LikeTweetResponseDto;
+import clone.twitter.dto.response.UserResponseDto;
 import clone.twitter.service.LikeTweetService;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.Link;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -50,11 +59,74 @@ public class LikeTweetController {
     }
 
     /**
+     * 개별 트윗에 좋아요를 표시한 유저목록의 최초 조회 요청에 응답합니다.
+     */
+    @GetMapping("/like/users")
+    public ResponseEntity<CollectionModel<UserResponseModel>> getUsersLikedTweet(@PathVariable String tweetId) {
+        List<UserResponseDto> userResponseDtos = likeTweetService.getUsersLikedTweet(tweetId);
+
+        String docsFragmentIdentifier = "users-liked-tweet";
+
+        CollectionModel<UserResponseModel> userResponseCollectionModel = convertToCollectionModel(userResponseDtos, docsFragmentIdentifier);
+
+        userResponseCollectionModel.add(linkTo(methodOn(LikeTweetController.class).getUsersLikedTweet(tweetId)).withSelfRel());
+
+        userResponseCollectionModel.add(linkTo(methodOn(LikeTweetController.class).getMoreUsersLikedTweet(tweetId, userResponseDtos.get(userResponseDtos.size() - 1).getUserId())).withRel("more-users-liked-tweet"));
+
+        userResponseCollectionModel.add(linkTo(methodOn(TweetController.class).getTweet(tweetId)).withRel("tweet"));
+
+        return ResponseEntity.ok(userResponseCollectionModel);
+    }
+
+    /**
+     * 개별 트윗에 좋아요를 표시한 유저목록의 추가 조회 요청에 응답합니다. (self의 경우 어떤 링크를 줘야하는지?그냥 본 메서드의 링크를 주면 현재 단계에서 더 로드를 하는 게 아닌지...)
+     */
+    @GetMapping("/like/users/{userIdLastOnList}")
+    public ResponseEntity<CollectionModel<UserResponseModel>> getMoreUsersLikedTweet(@PathVariable String tweetId, @PathVariable String userIdLastOnList) {
+        List<UserResponseDto> userResponseDtos = likeTweetService.getMoreUserLikedTweet(tweetId, userIdLastOnList);
+
+        if (!userResponseDtos.isEmpty()) {
+            String docsFragmentIdentifier = "more-users-liked-tweet";
+
+            CollectionModel<UserResponseModel> userResponseCollectionModel = convertToCollectionModel(userResponseDtos, docsFragmentIdentifier);
+
+            userResponseCollectionModel.add(linkTo(methodOn(TweetController.class).getTweet(tweetId)).withRel("tweet"));
+
+            return ResponseEntity.ok(userResponseCollectionModel);
+        }
+
+        return ResponseEntity.noContent().build();
+    }
+
+    /**
      * (내부용 메서드)개별 '좋아요'에 대한 정보를 조회합니다.
      */
     public LikeTweet getLikeTweet(String tweetId, String userId) {
         Optional<LikeTweet> optionalLike = likeTweetService.getLikeTweet(tweetId, userId);
 
         return optionalLike.orElse(null);
+    }
+
+    /**
+     * (controller 클래스 내부용 메서드, 향후 별도 클래스로 리팩토링 예정)UserResponseDto 리스트를 EntityModel 및 CollectionModel로 wrapping하고, 'fagmentIdentifier'를 docs의 url로 지정 및 추가하여 반환합니다. 향후 entity model에 관련한 기능들만 별도 branch로 만들어 본 메서드와 함께 refactoring 예정입니다(최상위 EntityModel class나 interface를 만들고 메서드로 포함 -> 하위 EntityModel class들이 상속하거나 구현(implement)하는 형태로 시도 예정)
+     */
+    private CollectionModel<UserResponseModel> convertToCollectionModel(
+        List<UserResponseDto> userResponseDtos, String fragmentIdentifier) {
+        List<UserResponseModel> userResponseModels = userResponseDtos.stream()
+            .map(userResponseDto -> {
+                UserResponseModel userResponseModel = new UserResponseModel(userResponseDto);
+
+                // to be refactored to be method of UserResponseModel
+                userResponseModel.add(Link.of("/docs/index.html#" + fragmentIdentifier).withRel("profile"));
+
+                return userResponseModel;
+            })
+            .collect(Collectors.toList());
+
+        CollectionModel<UserResponseModel> userResponseModelCollectionModel = CollectionModel.of(userResponseModels);
+
+        userResponseModelCollectionModel.add(Link.of("/docs/index.html#" + fragmentIdentifier).withRel("profile"));
+
+        return userResponseModelCollectionModel;
     }
 }
--- src/test/java/clone/twitter/controller/LikeTweetControllerTest.java
+++ src/test/java/clone/twitter/controller/LikeTweetControllerTest.java
@@ -8,6 +8,7 @@ import static org.springframework.restdocs.hypermedia.HypermediaDocumentation.li
 import static org.springframework.restdocs.hypermedia.HypermediaDocumentation.links;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.delete;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
 import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
@@ -22,18 +23,24 @@ import clone.twitter.common.BaseControllerTest;
 import clone.twitter.domain.LikeTweet;
 import clone.twitter.domain.Tweet;
 import clone.twitter.domain.User;
+import clone.twitter.dto.response.UserResponseDto;
 import clone.twitter.repository.LikeTweetRepository;
 import clone.twitter.repository.TweetRepository;
 import clone.twitter.repository.UserRepository;
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.IntStream;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 
 public class LikeTweetControllerTest extends BaseControllerTest {
     @Autowired
@@ -44,6 +51,13 @@ public class LikeTweetControllerTest extends BaseControllerTest {
 
     @Autowired
     TweetRepository tweetRepository;
+
+    @Autowired
+    LikeTweetController likeTweetController;
+
+    static final int NUMBER_OF_USERS = 10;
+
+    static final int NUMBER_OF_LIKE_TWEETS = 10;
 
     static final int BEGINNING_INDEX_OF_STREAM_RANGE = 0;
 
@@ -144,6 +158,154 @@ public class LikeTweetControllerTest extends BaseControllerTest {
                     fieldWithPath("_links.profile.href").description("documentation link to the api profile")
                 )
             ));
+    }
+
+    @Test
+    @DisplayName("GET /tweets/{tweetId}/like/users - 좋아요 표시 유저목록 조회 최초 요청")
+    void getUsersLikedTweet() throws Exception {
+        // given
+        // 10개의 유저 계정 생성
+        List<User> users = IntStream.range(BEGINNING_INDEX_OF_STREAM_RANGE, NUMBER_OF_USERS)
+            .mapToObj(i -> this.generateUser(i, BASE_CREATED_AT))
+            .toList();
+
+        // 첫 번째 유저가 1개의 트윗 생성
+        Tweet tweet = this.generateTweet(BEGINNING_INDEX_OF_STREAM_RANGE, users.get(BEGINNING_INDEX_OF_STREAM_RANGE).getId(), BASE_CREATED_AT);
+
+        // 작성자 포함 모든 유저 10명이 트윗 1개에 좋아요 표시
+        List<LikeTweet> likeTweets = IntStream.range(BEGINNING_INDEX_OF_STREAM_RANGE,
+                NUMBER_OF_LIKE_TWEETS)
+            .mapToObj(i -> this.generateLikeTweet(i, users.get(i).getId(), tweet.getId(), BASE_CREATED_AT))
+            .toList();
+
+        // when & then
+        this.mockMvc.perform(get("/tweets/{tweetId}/like/users", tweet.getId())
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .accept(MediaTypes.HAL_JSON_VALUE))
+            .andDo(print())
+            .andExpect(status().isOk())
+            .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaTypes.HAL_JSON_VALUE))
+            .andExpect(jsonPath("$._embedded.userResponseDtoList[*].userId").isNotEmpty())
+            .andExpect(jsonPath("$._embedded.userResponseDtoList[*].username").isNotEmpty())
+            .andExpect(jsonPath("$._embedded.userResponseDtoList[*].profileName").isNotEmpty())
+            .andExpect(jsonPath("$._embedded.userResponseDtoList[*].createdDate").isNotEmpty())
+            .andExpect(jsonPath("$._embedded.userResponseDtoList[*]._links.user-profile.href").isNotEmpty())
+            .andExpect(jsonPath("$._embedded.userResponseDtoList[*]._links.timeline.href").isNotEmpty())
+            .andExpect(jsonPath("$._embedded.userResponseDtoList[*]._links.profile.href").isNotEmpty())
+            .andExpect(jsonPath("$._links.profile.href").isNotEmpty())
+            .andExpect(jsonPath("$._links.self.href").isNotEmpty())
+            .andExpect(jsonPath("$._links.more-users-liked-tweet.href").isNotEmpty())
+            .andExpect(jsonPath("$._links.tweet.href").isNotEmpty())
+            .andDo(document("get-users-who-liked-tweet",
+                links(
+                    linkWithRel("profile").description("documentation link to the api profile"),
+                    linkWithRel("self").description("link to self(load initial users who liked the tweet)"),
+                    linkWithRel("more-users-liked-tweet").description("link to more users who liked the tweet"),
+                    linkWithRel("tweet").description("link to the tweet")
+                ),
+                requestHeaders(
+                    headerWithName(HttpHeaders.ACCEPT).description("accept header"),
+                    headerWithName(HttpHeaders.CONTENT_TYPE).description("content type header")
+                ),
+                pathParameters(
+                    parameterWithName("tweetId").description("identifier of individual tweet")
+                ),
+                responseHeaders(
+                    headerWithName(HttpHeaders.CONTENT_TYPE).description("content type of content(HAL-Json)")
+                ),
+                responseFields(
+                    fieldWithPath("_embedded.userResponseDtoList[*].userId").description("identifier of user account"),
+                    fieldWithPath("_embedded.userResponseDtoList[*].username").description("username of user account"),
+                    fieldWithPath("_embedded.userResponseDtoList[*].profileName").description("profile name of user account"),
+                    fieldWithPath("_embedded.userResponseDtoList[*].createdDate").description("created date of user account"),
+                    fieldWithPath("_embedded.userResponseDtoList[*]._links.user-profile.href").description("link to user profile page"),
+                    fieldWithPath("_embedded.userResponseDtoList[*]._links.timeline.href").description("link to timeline tweet list"),
+                    fieldWithPath("_embedded.userResponseDtoList[*]._links.profile.href").description("documentation link to the user info api profile"),
+                    fieldWithPath("_links.profile.href").description("documentation link to the api profile"),
+                    fieldWithPath("_links.self.href").description("link to self(load initial list of users who liked the tweet)"),
+                    fieldWithPath("_links.more-users-liked-tweet.href").description("link to more list of users who liked the tweet"),
+                    fieldWithPath("_links.tweet.href").description("link to the tweet")
+                )
+            ));
+    }
+
+    @Test
+    @DisplayName("GET /tweets/{tweetId}/like/users/{userIdLastOnList} - 좋아요 표시 유저목록 조회 추가 요청")
+    void getMoreUsersLikedTweet() throws Exception {
+        // given
+        // 10개의 유저 계정 생성
+        List<User> users = IntStream.range(BEGINNING_INDEX_OF_STREAM_RANGE, NUMBER_OF_USERS)
+            .mapToObj(i -> this.generateUser(i, BASE_CREATED_AT))
+            .toList();
+
+        // 첫 번째 유저가 1개의 트윗 생성
+        Tweet tweet = this.generateTweet(BEGINNING_INDEX_OF_STREAM_RANGE, users.get(BEGINNING_INDEX_OF_STREAM_RANGE).getId(), BASE_CREATED_AT);
+
+        // 작성자 포함 모든 유저 10명이 트윗 1개에 좋아요 생성
+        List<LikeTweet> likeTweets = IntStream.range(BEGINNING_INDEX_OF_STREAM_RANGE,
+                NUMBER_OF_LIKE_TWEETS)
+            .mapToObj(i -> this.generateLikeTweet(i, users.get(i).getId(), tweet.getId(), BASE_CREATED_AT))
+            .toList();
+
+        // 좋아요를 표시한 유저목록 최초 조회 요청시 받아온 유저목록의 마지막에 있는 유저 id 추출
+        ResponseEntity<CollectionModel<UserResponseModel>> usersLikedTweetResponseEntity = likeTweetController.getUsersLikedTweet(tweet.getId());
+
+        CollectionModel<UserResponseModel> collectionModel = usersLikedTweetResponseEntity.getBody();
+
+        assert collectionModel != null;
+
+        List<UserResponseModel> userResponseModelList = new ArrayList<>(collectionModel.getContent());
+
+        UserResponseModel lastUserResponseModel = userResponseModelList.get(userResponseModelList.size() - 1);
+
+        UserResponseDto lastUserResponseDto = lastUserResponseModel.getContent();
+
+        assert lastUserResponseDto != null;
+
+        // when & then
+        this.mockMvc.perform(get("/tweets/{tweetId}/like/users/{userIdLastOnList}", tweet.getId(), lastUserResponseDto.getUserId())
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .accept(MediaTypes.HAL_JSON_VALUE))
+            .andDo(print())
+            .andExpect(status().isOk())
+            .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaTypes.HAL_JSON_VALUE))
+            .andExpect(jsonPath("$._embedded.userResponseDtoList[*].userId").isNotEmpty())
+            .andExpect(jsonPath("$._embedded.userResponseDtoList[*].username").isNotEmpty())
+            .andExpect(jsonPath("$._embedded.userResponseDtoList[*].profileName").isNotEmpty())
+            .andExpect(jsonPath("$._embedded.userResponseDtoList[*].createdDate").isNotEmpty())
+            .andExpect(jsonPath("$._embedded.userResponseDtoList[*]._links.user-profile.href").isNotEmpty())
+            .andExpect(jsonPath("$._embedded.userResponseDtoList[*]._links.timeline.href").isNotEmpty())
+            .andExpect(jsonPath("$._embedded.userResponseDtoList[*]._links.profile.href").isNotEmpty())
+            .andExpect(jsonPath("$._links.profile.href").isNotEmpty())
+            .andExpect(jsonPath("$._links.tweet.href").isNotEmpty())
+            .andDo(document("get-more-users-who-liked-tweet",
+                links(
+                    linkWithRel("profile").description("documentation link to the api profile"),
+                    linkWithRel("tweet").description("link to the tweet")
+                ),
+                requestHeaders(
+                    headerWithName(HttpHeaders.ACCEPT).description("accept header"),
+                    headerWithName(HttpHeaders.CONTENT_TYPE).description("content type header")
+                ),
+                pathParameters(
+                    parameterWithName("tweetId").description("identifier of individual tweet"),
+                    parameterWithName("userIdLastOnList").description("identifier of the last user of previously loaded tweet list")
+                ),
+                responseHeaders(
+                    headerWithName(HttpHeaders.CONTENT_TYPE).description("content type of content(HAL-Json)")
+                ),
+                responseFields(
+                    fieldWithPath("_embedded.userResponseDtoList[*].userId").description("identifier of user account"),
+                    fieldWithPath("_embedded.userResponseDtoList[*].username").description("username of user account"),
+                    fieldWithPath("_embedded.userResponseDtoList[*].profileName").description("profile name of user account"),
+                    fieldWithPath("_embedded.userResponseDtoList[*].createdDate").description("created date of user account"),
+                    fieldWithPath("_embedded.userResponseDtoList[*]._links.user-profile.href").description("link to user profile page"),
+                    fieldWithPath("_embedded.userResponseDtoList[*]._links.timeline.href").description("link to timeline tweet list"),
+                    fieldWithPath("_embedded.userResponseDtoList[*]._links.profile.href").description("documentation link to the user info api profile"),
+                    fieldWithPath("_links.profile.href").description("documentation link to the api profile"),
+                    fieldWithPath("_links.tweet.href").description("link to the tweet")
+                )
+            ));;
     }
 
     @Test
