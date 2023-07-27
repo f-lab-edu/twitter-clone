bd124358c7ad5075ad5de4caabaea742fc72dcfd
JO <71416000+insightp25@users.noreply.github.com>
2023-07-27 17:42:10 +0900

    [#50] 피드백 반영 - 사용자계정 soft delete 구현

--- src/main/java/clone/twitter/controller/UserController.java
+++ src/main/java/clone/twitter/controller/UserController.java
@@ -75,7 +75,7 @@ public class UserController {
         return RESPONSE_OK;
     }
 
-    @DeleteMapping("/{userId}")
+    @PostMapping("/{userId}/inactivate")
     public ResponseEntity<Void> deleteUserAccount(@PathVariable String userId, @RequestParam String inputPassword) {
         if (userService.deleteUserAccount(userId, inputPassword)) {
             return RESPONSE_OK;
@@ -99,3 +99,16 @@ public class UserController {
     }
 }
 
+/*
+ - 사용자 계정 삭제기능에 soft-delete를 적용하였습니다.
+ - DB의 데이터를 hard-delete를 할 시:
+   - 해당 엔티티와 참조 관계가 있던 다른 테이블의 엔티티에 영향을 끼쳐 데이터의 정합성에 문제가 생길 수도 있고,
+   - 만약 사용자가 데이터를 삭제를 실수로 하였거나, 변심으로 복구를 원할 시 복원이 불가합니다.
+   - 또한 비즈니스나 제품 기능 개선에 효용을 제공하는 정보들을 잃게 된다는 측면도 있습니다.
+ - 반면 soft-delete로 구현 시:
+   - 상기의 문제를 해결할 수 있지만,
+   - 테이블에 삭제 상태를 표현하는 컬럼이 하나가 추가되어 데이터량이 증가하며(MySQL의 boolean/tinyint(1)은 1byte)
+   - 향후 조회등 쿼리에 있어 모든 데이터에 대해 삭제 상태인지를 확인하는 쿼리가 추가되어 complexity가 증가합니다.
+ - 두 방법 외 다른 선택지들에는 archiving, data anonymization, delayed hard-delete,
+   log-based deletion 등이 있습니다.
+ */
