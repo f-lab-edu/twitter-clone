5034d1a03e037c9692a5b7b9f2f5e05aebbfe08b
JO <71416000+insightp25@users.noreply.github.com>
2023-08-29 17:04:41 +0900

    [#55] Lombok API 설명 제거(WIKI의 Team Prerequsites항목으로 이동)

--- src/main/java/clone/twitter/domain/Tweet.java
+++ src/main/java/clone/twitter/domain/Tweet.java
@@ -22,14 +22,3 @@ public class Tweet {
 
     private LocalDateTime createdAt;
 }
-
-/*
- Lombok 라이브러리 용처 설명
- - `@Getter`: 객체 정보를 받아올 수 있도록 합니다.
- - `@Builder`: 보다 직관직이고 명시적인 방식으로 객체를 생성할 수 있습니다. `@...Constructor` 등을 통해
- 허용한 생성자 형식대로 사용할 수 있습니다.
- - `@NoArgsConstructor`: 모든 필드가 빈 객체 생성을 허용하며 객체 역질렬화 시 주로 사용됩니다.
- - `@AllArgsConstructor`: 모든 필드에 값이 할당되는 객체 생성을 허용합니다.
- - `@EqualsAndHashCode`: 객체의 논리 동치성을 확인하는 기준을 명시하며, 객체별로 유니크한 필드인 동시에
- 다른 객체를 참조하지 않는 필드를 비교 기준으로 지정함으로써 비교시 객체 상호순환 참조를 방지할 수 있습니다.
- */
--- src/main/java/clone/twitter/dto/response/FollowResponseDto.java
+++ src/main/java/clone/twitter/dto/response/FollowResponseDto.java
@@ -24,11 +24,4 @@ public class FollowResponseDto {
  `isFollowing`필드를 `@JsonProperty("isFollowing")와 같이 명시한 이유
  - `is...`의 형태가 Jackson의 기본 네이밍 문법/규칙이기 때문에, 직렬화/역직렬화 시
  해당 네이밍 규칙으로 쓰여진 필드를 필드로서 인식하지 않아 필드값이 무시되기 때문입니다.
-
- Lombok 라이브러리 용처 설명
- - `@Getter`: 객체 정보를 받아올 수 있도록 합니다.
- - `@Builder`: 보다 직관직이고 명시적인 방식으로 객체를 생성할 수 있습니다. `@...Constructor` 등을 통해
- 허용한 생성자 형식대로 사용할 수 있습니다.
- - `@NoArgsConstructor`: 모든 필드가 빈 객체 생성을 허용하며 객체 역질렬화 시 주로 사용됩니다.
- - `@AllArgsConstructor`: 모든 필드에 값이 할당되는 객체 생성을 허용합니다.
- */
+*/
