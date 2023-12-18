7209eaacd7d9a7a4dd673a5b59c73163279ac87f
JO <71416000+insightp25@users.noreply.github.com>
2023-12-18 13:27:22 +0900

    [#77] Redis Session 설정 분리, Qualifier 애노테이션 설정 수정, MyBatisConfig typo … …정정, 의존성 라이브러리 업데이트(jpa제거, aop 추가) - 2

--- src/main/java/clone/twitter/config/RedisConfig.java
+++ src/main/java/clone/twitter/config/RedisConfig.java
@@ -5,6 +5,7 @@ import static clone.twitter.util.CacheConstant.CACHE_DURATION_IN_HOUR;
 import java.time.Duration;
 import java.util.HashMap;
 import java.util.Map;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -22,48 +23,17 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @Configuration
 public class RedisConfig {
 
-    /* 테스트 v.3.1: redis session storage 제거
+    @Value("${spring.redis.fan-out.host}")
+    private String redisFanOutHost;
 
-    @Value("${spring.redis.session.host}")
-    private String redisSessionHost;
+    @Value("${spring.redis.fan-out.port}")
+    private int redisFanOutPort;
 
-    @Value("${spring.redis.session.port}")
-    private int redisSessionPort;
-
-    @Value("${spring.redis.session.password}")
-    private String redisSessionPassword;
-     */
-
-//    @Value("${spring.redis.fan-out.host}")
-    private String redisFanOutHost = "127.0.0.1";
-
-//    @Value("${spring.redis.fan-out.port}")
-    private int redisFanOutPort = 6379;
-
-//    @Value("${spring.redis.fan-out.password}")
-    private String redisFanOutPassword = "";
-
-    /*
-    // 테스트 v.3.1: redis session storage 제거
-    //
-    // spring-session-data-redis 의존성 추가시 해당 라이브러리가 "redisConnectionFactory"라는
-    // 이름으로 빈을 자동 등록하게 됩니다. 여기에서 다른 redis connection factory들과 이름 혼동이
-    // 올 수 있기에 명시적으로 "redisSessionConnectionFactory"라는 빈 이름을 추가등록하였습니다.
-    @Bean({"redisConnectionFactory", "redisSessionConnectionFactory"})
-    public RedisConnectionFactory redisSessionConnectionFactory() {
-
-        RedisStandaloneConfiguration redisStandaloneConfiguration
-                = new RedisStandaloneConfiguration();
-
-        redisStandaloneConfiguration.setHostName(redisSessionHost);
-        redisStandaloneConfiguration.setPort(redisSessionPort);
-        redisStandaloneConfiguration.setPassword(redisSessionPassword);
-
-        return new LettuceConnectionFactory(redisStandaloneConfiguration);
-    }
-     */
+    @Value("${spring.redis.fan-out.password}")
+    private String redisFanOutPassword;
 
     @Bean
+    @Qualifier(value = "redisFanOutConnectionFactory")
     public RedisConnectionFactory redisFanOutConnectionFactory() {
 
         RedisStandaloneConfiguration redisStandaloneConfiguration
@@ -78,12 +48,11 @@ public class RedisConfig {
 
     @Bean
     public RedisTemplate<String, Object> objectFanOutRedisTemplate(
+        @Qualifier(value = "redisFanOutConnectionFactory")
         RedisConnectionFactory redisFanOutConnectionFactory) {
 
         RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
 
-        // connection factory를 인수로 받아 템플릿에 설정하던 형식에서,
-        // 빈 등록시 설정에 의해 고정된 connection factory를 사용하는 형식으로 변경
         redisTemplate.setConnectionFactory(redisFanOutConnectionFactory);
 
         redisTemplate.setKeySerializer(new StringRedisSerializer());
@@ -96,12 +65,11 @@ public class RedisConfig {
 
     @Bean
     public RedisTemplate<String, String> stringFanOutRedisTemplate(
+        @Qualifier(value = "redisFanOutConnectionFactory")
         RedisConnectionFactory redisFanOutConnectionFactory) {
 
         RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
 
-        // connection factory를 인수로 받아 템플릿에 설정하던 형식에서,
-        // 빈 등록시 설정에 의해 고정된 connection factory를 사용하는 형식으로 변경
         redisTemplate.setConnectionFactory(redisFanOutConnectionFactory);
 
         redisTemplate.setKeySerializer(new StringRedisSerializer());
@@ -112,24 +80,24 @@ public class RedisConfig {
         return redisTemplate;
     }
 
-    /*
-    // redis cache 사용 보류
-    //
     @Bean
     public RedisCacheManager cacheManager(
-            RedisConnectionFactory connectionFactory) {
+        @Qualifier(value = "redisFanOutConnectionFactory")
+        RedisConnectionFactory connectionFactory) {
 
-        RedisCacheConfiguration defaultConfig = RedisCacheConfiguration.defaultCacheConfig()
+        RedisCacheConfiguration defaultConfig
+            = RedisCacheConfiguration.defaultCacheConfig()
                 .serializeValuesWith(RedisSerializationContext
-                        .SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()));
+                        .SerializationPair.fromSerializer(
+                            new GenericJackson2JsonRedisSerializer()));
 
         Map<String, RedisCacheConfiguration> redisCacheConfigMap = new HashMap<>();
 
-        redisCacheConfigMap.put("post", defaultConfig.entryTtl(Duration.ofHours(CACHE_DURATION_IN_HOUR)));
+        redisCacheConfigMap.put(
+            "post", defaultConfig.entryTtl(Duration.ofHours(CACHE_DURATION_IN_HOUR)));
 
         return RedisCacheManager.builder(connectionFactory)
                 .withInitialCacheConfigurations(redisCacheConfigMap)
                 .build();
     }
-     */
 }
